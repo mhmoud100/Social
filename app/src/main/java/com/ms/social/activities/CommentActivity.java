e7cb8b7d2dcd7790195681172cbecd8e1e053d34
mhmoud41 <mhmoudsoliman41@yahoo.com>
2020-11-20 15:16:28 +0200

    Post Update Commit

---
+++ app/src/main/java/com/ms/social/activities/CommentActivity.java
@@ -0,0 +1,16 @@
+package com.ms.social.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ms.social.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
---
+++ app/src/main/java/com/ms/social/model/Comment.java
@@ -0,0 +1,26 @@
+package com.ms.social.model;
+
+public class Comment {
+    String userId, messageText;
+
+    public Comment(String userId, String messageText) {
+        this.userId = userId;
+        this.messageText = messageText;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+}
